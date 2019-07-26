6abb8b53b0c5c373ea7379b7f979b1aa3e58892f
Ariel Saldana <hello@ariel.io>
2019-07-25 23:47:44 -0400

    image_controller: use name field as well

--- internal/controllers/image_controllers.go
+++ internal/controllers/image_controllers.go
@@ -20,30 +20,39 @@ func ImageCropController(w http.ResponseWriter, r *http.Request, ps httprouter.P
 	mw := getMagickWandUsingQueryParam(r)
 
 	var keys []string
-	keys = make([]string, 4)
+	keys = make([]string, 5)
+
 	keys[0] = "width"
 	keys[1] = "height"
 	keys[2] = "x"
 	keys[3] = "y"
+	keys[4] = "name"
 
 	operation := new(models.Operations)
+	operationName := "crop"
+	operation.Name = &operationName
+
 	queryParamsToOperations(r, operation, keys)
 
 	// logic
 	if operation.Width == nil {
-		*operation.Width = mw.GetImageWidth()
+		operationWidth := mw.GetImageWidth()
+		operation.Width = &operationWidth
 	}
 
 	if operation.Height == nil {
-		*operation.Height = mw.GetImageHeight()
+		operationHeight := mw.GetImageHeight()
+		operation.Height = &operationHeight
 	}
 
 	if operation.X == nil {
-		*operation.X = 0
+		operationX := uint(0)
+		operation.X = &operationX
 	}
 
 	if operation.Y == nil {
-		*operation.Y = 0
+		operationY := uint(0)
+		operation.Y = &operationY
 	}
 
 	mw.CropImage(*operation.Width, *operation.Height, int(*operation.X), int(*operation.Y))
@@ -77,27 +86,36 @@ func queryParamsToOperations(r *http.Request, operation *models.Operations, keys
 	queryValues := r.URL.Query()
 
 	for _, key := range keys {
-		var value = queryValues.Get(key)
-		var capitalizedKey = strings.Title(key)
+		var queryValue = queryValues.Get(key)
 
-		if len(value) == 0 {
+		if len(queryValue) == 0 {
 			continue
 		}
 
-		numericValue64, err := strconv.ParseUint(value, 10, 32)
-		numericValue := uint(numericValue64)
-
-		if err != nil {
-			panic(err)
-		}
+		var capitalizedKey = strings.Title(key)
+		var value interface{}
 
 		dynamicVariable := reflect.ValueOf(operation).Elem().FieldByName(capitalizedKey)
+		elementType := reflect.PtrTo(dynamicVariable.Type())
+		valueOfType := elementType.Elem().Elem().Kind().String()
+
+		if valueOfType == "string" {
+			value = queryValue
+		} else if valueOfType == "uint" {
+			numericValue64, err := strconv.ParseUint(queryValue, 10, 32)
+			if err != nil {
+				panic(err)
+			}
+			value = uint(numericValue64)
+		} else {
+			continue
+		}
 
 		if !dynamicVariable.IsValid() || !dynamicVariable.CanSet() {
 			continue
 		}
 
-		dynamicVariable.Set(ptr(reflect.ValueOf(numericValue)))
+		dynamicVariable.Set(ptr(reflect.ValueOf(value)))
 	}
 
 	return models.Operations{}
