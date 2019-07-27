9f718b455c5657a2b72e038d5e5ebee82bb72afd
Ariel Saldana <hello@ariel.io>
2019-07-26 21:35:56 -0400

    operation: removed pointer of values

--- internal/models/operation.go
+++ internal/models/operation.go
@@ -1,9 +1,9 @@
 package models
 
 type Operations struct {
-	Name   *string
-	Width  *uint
-	Height *uint
-	X      *uint
-	Y      *uint
+	Name   string
+	Width  uint
+	Height uint
+	X      uint
+	Y      uint
 }
